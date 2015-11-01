7078288fb4bb86428e2db5853d22f8fc0b4c21fa
Demjan Kaluzki <kaluzkidemjan@gmail.com>
2015-11-01 02:39:29 +0100

    check if ansible is installed

--- install.sh
+++ install.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
-echo "initial install script"
+command -v ansible >/dev/null 2>&1 || {
+  echo >&2 "
+Please install ansible first:
+
+sudo apt-get -y install software-properties-common
+sudo apt-add-repository -y ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get -y install ansible
+
+"; exit 1;
+}
+
+ansible all -i "localhost," -c local -b -m apt -a 'name=git state=latest'
+ansible all -i "localhost," -c local -m git -a 'repo=https://github.com/kaluzki/dotfiles.git dest=~/dotfiles'
