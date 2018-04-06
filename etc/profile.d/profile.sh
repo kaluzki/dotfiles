49c142ae1f5a2e2b851b676b72d230e52b60dcb7
Demjan Kaluzki <kaluzkidemjan@gmail.com>
2018-04-06 15:02:48 +0200

    add ~/bin ~/.local/bin to $PATH

--- etc/profile.d/profile.sh
+++ etc/profile.d/profile.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 [ -s "$HOME/etc/profile.d/aliases.sh" ] && . "$HOME/etc/profile.d/aliases.sh"
-PATH="$HOME/.yarn/bin:$HOME/anaconda3/bin:$PATH"
+PATH="$HOME/bin:$HOME/.local/bin:$HOME/.yarn/bin:$HOME/anaconda3/bin:$PATH"
 CDPATH=.:~:~/github:~/github/kaluzki:~/gitlab
